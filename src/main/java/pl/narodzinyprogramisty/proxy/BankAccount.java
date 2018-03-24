b2b9bc63cce970f59efc013e6cd712442e149dd2
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-03-24 10:47:12 +0100

    proxy pattern

---
+++ src/main/java/pl/narodzinyprogramisty/proxy/BankAccount.java
@@ -0,0 +1,32 @@
+package pl.narodzinyprogramisty.proxy;
+
+public class BankAccount implements BankOperation{
+    private String cardId;
+    private int saldo;
+
+    public BankAccount(String cardId) {
+        this.cardId = cardId;
+        saldo = 500;
+    }
+
+    @Override
+    public void transfer(int amount) {
+        saldo -= amount;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+}
