2fe2ccea6d8b587e3bd1868458c37765b1c83811
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-04-07 10:28:52 +0200

    Facade pattern (#3)

---
+++ src/main/java/pl/narodzinyprogramisty/Fasade/Main.java
@@ -0,0 +1,9 @@
+package pl.narodzinyprogramisty.Fasade;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TravelRequest travelRequest = new TravelRequest();
+        travelRequest.bookTravel();
+    }
+}
